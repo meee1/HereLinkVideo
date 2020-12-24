375c88094b78ace1db3c0e09f8187e90f761d7c6
Michael Oborne <mich146@hotmail.com>
2020-12-24 16:44:40 +0800

    Add project files.

---
+++ HereLinkVideo.Android/FormsVideoLibrary/VideoPicker.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Threading.Tasks;
+using Android.Content;
+using HereLinkVideo.Droid;
+
+using Xamarin.Forms;
+
+
+
+namespace FormsVideoLibrary.Droid
+{
+
+}
---
+++ HereLinkVideo/Video.xaml.cs
@@ -0,0 +1,15 @@
+﻿
+using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace HereLinkVideo
+{
+    public partial class Video : ContentPage
+    {
+        public Video()
+        {
+            InitializeComponent();
+        }
+    }
+}
